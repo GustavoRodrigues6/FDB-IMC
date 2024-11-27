d93c212992a02d3c604710cdd52707be62579e3d
Gustavo Rodrigues <gurodriguesalves12@gmail.com>
2024-11-27 09:18:05 +0000

    Updated

--- main.py
+++ main.py
@@ -3,19 +3,29 @@ import imc
 from users import *
 from imc import *
 
-# Solicita os dados para criar um utilizador
-print("--------------------------- Registo ---------------------------")
-username = input("Nome do utilizador: ")
-age = int(input("Idade do utilizador: "))
-height = int(input("Altura do utilizador (em cm): "))
-weight = int(input("Peso do utilizador (em kg): "))
+def main():
+    while True:
+        print("--------------------------- Menu ---------------------------")
+        print("1. Registar Utilizador")
+        print("2. Calcular IMC")
+        print("3. Sair")
+        choice = input("Escolha uma opção: ")
 
-# Chama a função para criar o utilizador com os dados fornecidos
-create_user(username, age, height, weight)
+        if choice == '1':
+            print("--------------------------- Registo ---------------------------")
+            username = input("Nome do utilizador: ")
+            age = int(input("Idade do utilizador: "))
+            height = int(input("Altura do utilizador (em cm): "))
+            weight = int(input("Peso do utilizador (em kg): "))
+            create_user(username, age, height, weight)
+        elif choice == '2':
+            username_input = input("Digite o nome do utilizador: ")
+            calcular_imc(username_input)
+        elif choice == '3':
+            print("Saindo...")
+            break
+        else:
+            print("Opção inválida! Tente novamente.")
 
-# Pergunta o nome do utilizador
-username_input = input("Digite o nome do utilizador para calcular o seu IMC: ")
-
-# Chama a função para calcular o IMC com o nome fornecido
-calcular_imc(username_input)
-
+if __name__ == "__main__":
+    main()
